43003b6f861b1606d604dbcd072254919339b1bb
Frederico Mendonca Franca <fredfranssa@gmail.com>
2024-01-02 23:29:45 -0300

    Starting

---
+++ dist/precache-manifest.32955cdcd2db3dec93f3a0815d36eb8d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28598fc0b4c785408c71426e695fd853",
+    "url": ".well-known/assetlinks.json"
+  },
+  {
+    "revision": "ded7c42ec62859f5149201979d812f87",
+    "url": "ads.txt"
+  },
+  {
+    "revision": "23b36253ca8afaa6b269",
+    "url": "css/app.2aae8569.css"
+  },
+  {
+    "revision": "de57d7874243acaec3d0",
+    "url": "css/chunk-vendors.08cdc53b.css"
+  },
+  {
+    "revision": "8b4321f782e84764e556af3dee32a131",
+    "url": "img/become_a_patron_button.png"
+  },
+  {
+    "revision": "e911a48d9e04eecfef3a9fc779f4c206",
+    "url": "index.html"
+  },
+  {
+    "revision": "23b36253ca8afaa6b269",
+    "url": "js/app.3c430535.js"
+  },
+  {
+    "revision": "de57d7874243acaec3d0",
+    "url": "js/chunk-vendors.15a8945c.js"
+  },
+  {
+    "revision": "1408be3e50dd59cfa054d30aa2ca6189",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "cd8d8116c8b49a753b68423aa0ace475",
+    "url": "privacy.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
